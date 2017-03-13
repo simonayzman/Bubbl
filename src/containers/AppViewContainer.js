ef4209c13044455cdb12a181fcdcb1bb698e1485
Simon Ayzman <simonayzman@gmail.com>
2017-03-13 00:36:09 -0400

    Updated app theme styles

--- src/containers/AppViewContainer.js
+++ src/containers/AppViewContainer.js
@@ -17,8 +17,56 @@ export default function AppViewContainer() {
 
 const customTheme = {
   defaultFont: {
-    fontFamily: 'Helvetica',
+    fontFamily: 'Helvetica Neue',
   },
+
+  'shoutem.ui.Text': {
+    '.dark': {
+      color: 'white',
+    },
+  },
+
+  'shoutem.ui.View': {
+    '.dark': {
+      backgroundColor: 'black',
+    },
+  },
+
+  'shoutem.ui.Row': {
+    '.dark': {
+      backgroundColor: 'black',
+    },
+  },
+
+  'shoutem.ui.Tile': {
+    '.dark': {
+      backgroundColor: 'black',
+    },
+  },
+
+  'shoutem.ui.TouchableOpacity': {
+    '.dark': {
+      backgroundColor: 'black',
+    },
+  },
+
+  'shoutem.ui.Title': {
+    fontSize: 18,
+    lineHeight: 20,
+    letterSpacing: 0.5,
+  },
+
+  'shoutem.ui.Subtitle': {
+    fontSize: 15,
+    lineHeight: 18,
+  },
+
+  'shoutem.ui.Caption': {
+    fontSize: 10,
+    lineHeight: 16,
+    letterSpacing: 0.5,
+  },
+
   // 'shoutem.ui.Card': {
   //     // card component variants
   //     '.dark': {
