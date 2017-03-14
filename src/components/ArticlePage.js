0118fe78df5fd8b251ec82ef7e31d463189703f0
Simon Ayzman <simonayzman@gmail.com>
2017-03-14 17:02:41 -0400

    Fixed article page navigation

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -1,21 +1,23 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { WebView } from 'react-native';
 
-const ArticlePage = () => (
-  <WebView
-    source={{ uri: this.props.navigation.state.params.url }}
-    style={{ flex: 1, marginTop: 20 }}
-  />
-);
-
-ArticlePage.propTypes = {
-  navigation: PropTypes.shape({
-    state: PropTypes.shape({
-      params: PropTypes.shape({
-        url: PropTypes.string.isRequired,
+export default class ArticlePage extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      state: PropTypes.shape({
+        params: PropTypes.shape({
+          url: PropTypes.string.isRequired,
+        }).isRequired,
       }).isRequired,
     }).isRequired,
-  }).isRequired,
-};
+  };
 
-export default ArticlePage;
+  render() {
+    return (
+      <WebView
+        source={{ uri: this.props.navigation.state.params.url }}
+        style={{ flex: 1, marginTop: 20 }}
+      />
+    );
+  }
+}
